07b419e74b8d37e8721455eab07a2c19bdfec7ad
Victor <victormel87@gmail.com>
2016-03-31 10:35:02 +0200

    Creada clase BoxBall

---
+++ BoxBall.java
@@ -0,0 +1,93 @@
+import java.awt.*;
+import java.awt.geom.*;
+
+/**
+ * Write a description of class BoxBall here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class BoxBall
+{
+    private static final int GRAVITY = 3;  // effect of gravity
+
+    private int ballDegradation = 2;
+    private Ellipse2D.Double circle;
+    private Color color;
+    private int diameter;
+    private int xPosition;
+    private int yPosition;
+    private final int groundPosition;      // y position of ground
+    private Canvas canvas;
+    private int ySpeed = 1;                // initial downward speed
+    /**
+     * Constructor for objects of class BoxBall
+     */
+    public BoxBall(int xPos, int yPos, int ballDiameter, Color ballColor,
+                        int groundPos, Canvas drawingCanvas)
+    {
+        xPosition = xPos;
+        yPosition = yPos;
+        color = ballColor;
+        diameter = ballDiameter;
+        groundPosition = groundPos;
+        canvas = drawingCanvas;
+    }
+
+     /**
+     * Draw this ball at its current position onto the canvas.
+     **/
+    public void draw()
+    {
+        canvas.setForegroundColor(color);
+        canvas.fillCircle(xPosition, yPosition, diameter);
+    }
+
+    /**
+     * Erase this ball at its current position.
+     **/
+    public void erase()
+    {
+        canvas.eraseCircle(xPosition, yPosition, diameter);
+    }    
+
+    /**
+     * Move this ball according to its position and speed and redraw.
+     **/
+    public void move()
+    {
+        // remove from canvas at the current position
+        erase();
+            
+        // compute new position
+        ySpeed += GRAVITY;
+        yPosition += ySpeed;
+        xPosition +=2;
+
+        // check if it has hit the ground
+        if(yPosition >= (groundPosition - diameter) && ySpeed > 0) {
+            yPosition = (int)(groundPosition - diameter);
+            ySpeed = -ySpeed + ballDegradation; 
+        }
+
+        // draw again at new position
+        draw();
+    }    
+
+    /**
+     * return the horizontal position of this ball
+     */
+    public int getXPosition()
+    {
+        return xPosition;
+    }
+
+    /**
+     * return the vertical position of this ball
+     */
+    public int getYPosition()
+    {
+        return yPosition;
+    }
+}
+
